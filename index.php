d5b103c6c2f96dff875f26f3f68b4a18a93c8c85
Divyanjala <45142236+Divyanjala@users.noreply.github.com>
2019-02-11 13:49:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student management</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet"  type="text/css" href="css/style.css">
+
+</head>
+<body>
+    <div class="nav">
+        <div class="top">
+            <div class="abc">
+                <ul class=" a nav">
+                    <li class="nav-item">
+                        <a class=" aa nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class=" aa nav-link" href="#">Site Map</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="aa nav-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="aa nav-link" href="#">IJSE News</a>
+                    </li>
+                    </li>
+                    <li class="nav-item">
+                        <a class="aa nav-link" href="#">World Awards</a>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="aa nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About Us</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                            <a class="dropdown-item" href="#">Separated link</a>
+                        </div>
+                    </li>
+                    <div class="aaa">
+                        <li class="nav-item">
+                            <a class=" nav-link" href="#"> </a>
+                        </li>
+
+                    </div>
+                    <div class="log">
+                        <li class="nav-item">
+                            <a class="l nav-link" href="login.php">LOGIN</a>
+                        </li>
+
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="mainIm">
+        <div class="item">
+            <img src="assert/students-at-graduation.jpg" alt="Chicago">
+        </div>
+    </div>
+<div class="image">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="course.php">
+                    <img src="assert/technical-analysis-online-course.jpg" alt="Lights" style="width:100%">
+                    <div class="caption">
+                        <h3>IJSE COURSE</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="register.php">
+                    <img src="assert/Online registration photo.jpg" alt="Nature" style="width:100%" height="340">
+                    <div class="caption">
+                        <h3>STUDENT REGISTER</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="thumbnail">
+                <a href="#">
+                    <img src="assert/index.jpeg" alt="Fjords" style="width:100%" height="340">
+                    <div class="caption">
+                        <h3>IJSE AWARDS</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
